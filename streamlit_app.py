f21a9c716f8e62856429d90dfa7ebb26f0681a89
GitHubDDPP <135846737+GitHubDDPP@users.noreply.github.com>
2023-06-07 16:22:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,25 @@
 created main python file :streamlit_app.py
+
+Melanie's Menu Form (réponses) - Google Sheets
+docs.google.com/spreadsheets/d/1HDLH1B2dRlBbgkiryf-6D78oNtfXbQ9w8etu0CkFIsM/edit?resourcekey#gid=1010101036
+
+
+Fichier streamlit_app.py :
+import streamlit
+
+streamlit.title('My parents New Healthy Diner')
+
+##streamlit.header('Breakfast Menu')
+##streamlit.text('Omega 3 & Blueberry Oatmeal')
+##streamlit.text('Kale, Spinach & Rocket Smoothie')
+##streamlit.text('Hard-Boiled Free-Range Egg'*
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')  
+  
+  
