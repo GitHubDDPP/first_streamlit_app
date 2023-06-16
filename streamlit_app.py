8bbfe8c0f2723630c071e75270583a90d7a08a48
GitHubDDPP <135846737+GitHubDDPP@users.noreply.github.com>
2023-06-16 12:23:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,3 +65,5 @@ streamlit.dataframe(my_data_rows)
 # 16 juin
 add_my_fruit = streamlit.text_input('What fruit do you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into  FRUIT_LOAD_LIST values ('from streamlit')")
